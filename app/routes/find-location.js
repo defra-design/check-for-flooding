97a0257c628c07682e4db1c1efe6c962f5c8eb5a
danleech-defra <dan.leech@defra.net>
2021-06-18 16:40:53 +0100

    Stuff

--- app/routes/find-location.js
+++ app/routes/find-location.js
@@ -1,21 +1,61 @@
 const express = require('express')
 const router = express.Router()
-
-router.get('/location', (req, res) => {
-  res.redirect('/find-location')
-})
+const axios = require('axios')
+const apiKey = process.env.OS_NAMES_KEY
 
 router.get('/find-location', (req, res) => {
   res.render('find-location')
 })
 
-router.post('/find-location', (req, res) => {
-  const model = { location: req.body.location }
-  if (model.location) {
-    res.redirect(`/location/${model.location}`)
+router.post('/find-location', async (req, res) => {
+  const model = { query: req.body.location }
+  const locations = []
+  if (model.query === '') {
+    model.isError = true
+    return res.render('find-location', { model })
+  }
+  const types = ['postcode', 'hamlet', 'village', 'town', 'city', 'other_settlement'].map(i => `local_type:${i}`).join(' ')
+  const uri = `https://api.os.uk/search/names/v1/find?query=${model.query}&fq=${types}&key=${apiKey}`
+  const response = await axios.get(uri).then((response) => {
+    return response
+  })
+  if (response.status === 200) {
+    // Filter results to remove non England and where names dont match
+    const results = response.data.results && response.data.results.filter(result =>
+      result.GAZETTEER_ENTRY.COUNTRY === 'England' && result.GAZETTEER_ENTRY.NAME1 === response.data.results[0].GAZETTEER_ENTRY.NAME1)
+    // We have some matches
+    if (results.length) {
+      results.forEach(result => {
+        const gazetteerEntry = result.GAZETTEER_ENTRY
+        const localType = gazetteerEntry.LOCAL_TYPE
+        const name = gazetteerEntry.NAME1
+        const countyUnity = gazetteerEntry.COUNTY_UNITARY
+        const districtBorough = gazetteerEntry.DISTRICT_BOROUGH
+        // Construct the slug
+        const location = {}
+        if (localType === 'Postcode') {
+          location.slug = name.replace(/\s+/g, '-').toLowerCase()
+          location.name = `${name}, ${(countyUnity || districtBorough)}`
+        } else if (localType === 'City' || name === countyUnity || name === districtBorough) {
+          location.slug = name.replace(/\s+/g, '-').toLowerCase()
+          location.name = name
+        } else if (countyUnity || districtBorough) {
+          location.slug = `${name.replace(/\s+/g, '-').toLowerCase()}-${(countyUnity || districtBorough).replace(/\s+/g, '-').toLowerCase()}`
+          location.name = `${name}, ${(countyUnity || districtBorough)}`
+        }
+        locations.push(location)
+      })
+      model.locations = locations
+      console.log(model)
+      if (results.length === 1) {
+        res.redirect(`/location/${model.locations[0].slug}`)
+      } else {
+        res.render('choose-location', { model })
+      }
+    }
   } else {
-    model.error = true
-    res.render('find-location', { model })
+    // Return 500 eror
+    console.log('500 error')
   }
 })
 
--- app/routes/location.js
+++ app/routes/location.js
@@ -3,48 +3,47 @@ const router = express.Router()
 const axios = require('axios')
 const apiKey = process.env.OS_NAMES_KEY
 
-// Add your routes here - above the module.exports line
-router.get('/location/:location', (req, res) => {
+router.get('/location', (req, res) => {
+  res.redirect('/find-location')
+})
+
+router.get('/location/:location', async (req, res) => {
   const query = req.params.location.toLowerCase()
   const types = ['postcode', 'hamlet', 'village', 'town', 'city', 'other_settlement'].map(i => `local_type:${i}`).join(' ')
-  const uri = `https://api.os.uk/search/names/v1/find?query=${query}&fq=${types}&maxresults=5&key=${apiKey}`
-  async function find () {
-    axios.get(uri)
-      .then(function (response) {
-        // Successful response
-        if (response.status === 200) {
-          // Found a match
-          if (response.data.header.totalresults > 0) {
-            const gazetteerEntry = response.data.results[0].GAZETTEER_ENTRY
-            const country = gazetteerEntry.COUNTRY
-            const localType = gazetteerEntry.LOCAL_TYPE
-            const name = gazetteerEntry.NAME1
-            const countyUnity = gazetteerEntry.COUNTY_UNITARY
-            const districtBorough = gazetteerEntry.DISTRICT_BOROUGH
-            const model = {}
-            if (localType === 'Postcode') {
-              model.slug = name.replace(/\s+/g, '-').toLowerCase()
-              model.name = `${name}, ${(countyUnity || districtBorough)}`
-            } else if (localType === 'City' || name === countyUnity || name === districtBorough) {
-              model.slug = name.replace(/\s+/g, '-').toLowerCase()
-              model.name = name
-            } else if (countyUnity || districtBorough) {
-              model.slug = `${name.replace(/\s+/g, '-').toLowerCase()}-${(countyUnity || districtBorough).replace(/\s+/g, '-').toLowerCase()}`
-              model.name = `${name}, ${(countyUnity || districtBorough)}`
-            }
-            // We have a valid url
-            if (country === 'England' && query === model.slug) {
-              res.render('location', model)
-              return
-            }
-          }
-          // Return 404 error
-          res.status(404).render('404')
+  const uri = `https://api.os.uk/search/names/v1/find?query=${query}&fq=${types}&maxresults=1&key=${apiKey}`
+
+  axios.get(uri).then(function (response) {
+    // Successful response
+    if (response.status === 200) {
+      // Found a match
+      if (response.data.header.totalresults > 0) {
+        const gazetteerEntry = response.data.results[0].GAZETTEER_ENTRY
+        const country = gazetteerEntry.COUNTRY
+        const localType = gazetteerEntry.LOCAL_TYPE
+        const name = gazetteerEntry.NAME1
+        const countyUnity = gazetteerEntry.COUNTY_UNITARY
+        const districtBorough = gazetteerEntry.DISTRICT_BOROUGH
+        const model = {}
+        if (localType === 'Postcode') {
+          model.slug = name.replace(/\s+/g, '-').toLowerCase()
+          model.name = `${name}, ${(countyUnity || districtBorough)}`
+        } else if (localType === 'City' || name === countyUnity || name === districtBorough) {
+          model.slug = name.replace(/\s+/g, '-').toLowerCase()
+          model.name = name
+        } else if (countyUnity || districtBorough) {
+          model.slug = `${name.replace(/\s+/g, '-').toLowerCase()}-${(countyUnity || districtBorough).replace(/\s+/g, '-').toLowerCase()}`
+          model.name = `${name}, ${(countyUnity || districtBorough)}`
         }
-        // Return 500 error
-      })
-  }
-  find()
+        // Valid url and region
+        if (country === 'England' && query === model.slug) {
+          return res.render('location', model)
+        }
+      }
+      // Return 404 error
+      return res.status(404).render('404')
+    }
+    // Return 500 error
+  })
 })
 
 module.exports = router
