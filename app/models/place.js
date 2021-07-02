4ab9ac768d829f01c4d37821878c61479137d647
danleech-defra <dan.leech@defra.net>
2021-07-02 10:17:58 +0100

    Stuff

--- app/models/place.js
+++ app/models/place.js
@@ -6,13 +6,19 @@ class Place {
     const name = gazetteerEntry.NAME1
     const countyUnity = gazetteerEntry.COUNTY_UNITARY
     const districtBorough = gazetteerEntry.DISTRICT_BOROUGH
-    // Construct the slug and name
+    const postcodeDistrict = gazetteerEntry.POSTCODE_DISTRICT
+    const isSimilar = gazetteerEntry.IS_SIMILAR
+    // Construct the name
     if (localType === 'Postcode') {
       this.name = `${name}, ${(countyUnity || districtBorough)}`
     } else if (localType === 'City' || name === countyUnity || name === districtBorough) {
       this.name = name
     } else if (countyUnity || districtBorough) {
       this.name = `${name}, ${(countyUnity || districtBorough)}`
+    }
+    // Add additional qualifying compnent if names are similar
+    if (isSimilar) {
+      this.name = `${this.name} (${postcodeDistrict})`
     }
     // Crerate slug
     this.slug = utils.getSlugFromGazetteerEntry(gazetteerEntry)
