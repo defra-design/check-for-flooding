33278261280040c510afcdb6bd271c922ee85bc5
danleech-defra <dan.leech@defra.net>
2021-07-26 14:02:34 +0100

    Stuff

--- app/services/river.js
+++ app/services/river.js
@@ -2,14 +2,15 @@ const axios = require('axios')
 const utils = require('../utils')
 const serviceUrl = process.env.SERVICE_URL
 
-axios.defaults.baseURL = serviceUrl
-
 module.exports = {
   getRivers: async (query) => {
     const slug = utils.getSlug(query)
-    const uri = `${serviceUrl}/rivers/${slug}`
+    const uri = `/rivers/${slug}`
     try {
-      const response = await axios.get(uri)
+      const response = await axios.get(uri, {
+        method: 'get',
+        baseURL: serviceUrl
+      })
       return response
     } catch (error) {
       console.error(error)
