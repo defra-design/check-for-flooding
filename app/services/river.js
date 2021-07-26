08c1374f01dc7b5c58ac2011baaadf10e18bc136
danleech-defra <dan.leech@defra.net>
2021-07-26 09:37:50 +0100

    Stuff

--- app/services/river.js
+++ app/services/river.js
@@ -2,12 +2,12 @@ const axios = require('axios')
 const utils = require('../utils')
 const serviceUrl = process.env.SERVICE_URL
 
-axios.defaults.headers.post['Access-Control-Allow-Methods'] = 'GET'
+axios.defaults.baseURL = serviceUrl
 
 module.exports = {
   getRivers: async (query) => {
     const slug = utils.getSlug(query)
-    const uri = `${serviceUrl}/rivers/${slug}`
+    const uri = `/rivers/${slug}`
     console.log(uri)
     const response = await axios.get(uri).then((response) => { return response })
     return response
