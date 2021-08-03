f0792ce6c0f68c089563ef271e224786f12cfe91
danleech-defra <dan.leech@defra.net>
2021-08-03 10:22:36 +0100

    Stuff

--- app/models/river.js
+++ app/models/river.js
@@ -2,7 +2,9 @@ class River {
   constructor (data) {
     this.slug = data.slug
     this.name = data.name
-    this.bbox = data.bbox
+    this.bbox = data.bbox.split(',').map((value) => {
+      return Number(value)
+    })
     this.levels = data.levels
   }
 }
