e3203e4fa9e5174a0b389f58203fc604d01646b2
danleech-defra <dan.leech@defra.net>
2021-08-06 12:50:08 +0100

    Stuff

--- app/routes/levels.js
+++ app/routes/levels.js
@@ -85,79 +85,12 @@ router.get('/levels', async (req, res) => {
   }
 })
 
-// Get levels by location
-// router.get('/levels/location/:location', async (req, res) => {
-//   const slug = req.params.location.toLowerCase()
-//   let response = await locationServices.getLocation(slug)
-//   const model = {}
-
-//   // Get place
-//   if (response.status === 200) {
-//     if (response.data && response.data.result) {
-//       // We have a valid location
-//       model.place = new Place(response.data.result)
-//     } else {
-//       // Return 404
-//       return res.status(404).render('404')
-//     }
-//   } else {
-//     // Return 500 error
-//     return res.status(503).render('500')
-//   }
-
-//   // Get levels
-//   if (model.place) {
-//     response = await levelServices.getLevelsWithin(model.place.bboxBuffered)
-//     if (response.data) {
-//       const levels = utils.groupBy(response.data, 'group_name')
-//       Object.entries(levels).forEach(([key, value]) => {
-//         value.forEach((item, index) => {
-//           levels[key][index] = new Level(item)
-//         })
-//       })
-//       model.numLevels = response.data.length
-//       model.levels = levels
-//     }
-//   }
-
-//   return res.render('levels', { model })
-// })
-
-// Get levels by river
-// router.get('/levels', async (req, res) => {
-//   const slug = req.params.river.toLowerCase()
-//   const response = await riverServices.getRiverDetail(slug)
-//   if (response.status === 200) {
-//     if (response.data) {
-//       // We have a valid route
-//       const river = new River(response.data[0])
-//       const levels = []
-//       response.data.forEach((item, index) => {
-//         if (index >= 1) {
-//           const level = new Level(item)
-//           levels.push(level)
-//         }
-//       })
-//       const model = {
-//         river: river,
-//         levels: levels
-//       }
-//       return res.render('levels', { model })
-//     } else {
-//       // Return 404
-//       return res.status(404).render('404')
-//     }
-//   } else {
-//     // Return 500 error
-//   }
-// })
-
 // Search levels
 router.post('/levels', async (req, res) => {
   const query = req.body.location
   const model = { query: query }
   if (query === '') {
-    model.isError = true
+    // model.isError = true
     return res.render('levels', { model })
   }
 
