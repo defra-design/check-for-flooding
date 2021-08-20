2b11e9677501879b321a3d424732b52fa24c0f3f
danleech-defra <dan.leech@defra.net>
2021-08-20 14:11:40 +0100

    Stuff

--- app/models/level.js
+++ app/models/level.js
@@ -4,10 +4,15 @@ class Level {
     this.name = data.name
     this.state = data.state
     this.value = data.value
-    this.velueDate = data.value_date
+    this.valueDownstream = data.value_downstream
+    this.value1hr = data.value_1hr
+    this.value6hr = data.value_6hr
+    this.value24hr = data.value_24hr
+    this.valueDate = data.value_date
     this.elapsedTime = data.value_date ? this.formatElaspedTime(data.value_date) : ''
     this.type = data.type
     this.riverSlug = data.river_slug || ''
+    this.isDownstream = Boolean(data.is_downstream)
   }
 
   formatElaspedTime (date) {
--- service/services.js
+++ service/services.js
@@ -47,23 +47,46 @@ module.exports = {
   getStationsWithinBbox: async (bbox) => {
     // Convert type names to chars
     const response = await db.query(`
-    SELECT station.name,
+    (SELECT station.name,
     station.id,
     station.state,
     station.value,
+    station.value_downstream,
+    station.value_1hr,
+    station.value_6hr,
+    station.value_24hr,
     station.value_date,
     station.type_name AS type,
-    CASE WHEN river.display is NOT NULL THEN river.display
-    ELSE station.name
-    END AS group_name,
-    CASE WHEN river.display is NOT NULL THEN river.slug
-    ELSE NULL
-    END AS river_slug
+    CASE WHEN station.type = 'm' THEN 1 ELSE 0 END AS is_multi,
+    CASE WHEN river.display is NOT NULL THEN river.display ELSE station.name END AS group_name,
+    CASE WHEN river.display is NOT NULL THEN river.slug ELSE NULL END AS river_slug,
+    river_station.order AS station_order,
+    0 AS is_downstream
     FROM river
     RIGHT JOIN river_station ON river_station.slug = river.slug
     RIGHT JOIN station ON river_station.station_id = station.id
-    WHERE ST_Contains(ST_MakeEnvelope($1,$2,$3,$4,4326),station.geom)
-    ORDER BY group_name, river_station.order  
+    WHERE ST_Contains(ST_MakeEnvelope($1,$2,$3,$4,4326),station.geom))
+    UNION ALL
+    (SELECT station.name,
+    station.id,
+    station.state,
+    station.value,
+    station.value_downstream,
+    station.value_1hr,
+    station.value_6hr,
+    station.value_24hr,
+    station.value_date,
+    station.type_name AS type,
+    CASE WHEN station.type = 'm' THEN 1 ELSE 0 END AS is_multi,
+    CASE WHEN river.display is NOT NULL THEN river.display ELSE station.name END AS group_name,
+    CASE WHEN river.display is NOT NULL THEN river.slug ELSE NULL END AS river_slug,
+    river_station.order AS station_order,
+    1 AS is_downstream
+    FROM river
+    RIGHT JOIN river_station ON river_station.slug = river.slug
+    RIGHT JOIN station ON river_station.station_id = station.id
+    WHERE (ST_Contains(ST_MakeEnvelope($1,$2,$3,$4,4326),station.geom)) AND station.type = 'm')
+    ORDER BY group_name, station_order, is_downstream  
     `, bbox)
     return response.rows || {}
   },
@@ -72,23 +95,44 @@ module.exports = {
   getStationsByRiverSlug: async (slug) => {
     // Convert type names to chars
     const response = await db.query(`
-    SELECT station.name,
+    (SELECT station.name,
     station.id,
     station.state,
     station.value,
+    station.value_downstream,
+    station.value_1hr,
+    station.value_6hr,
+    station.value_24hr,
     station.value_date,
     station.type_name AS type,
-    CASE WHEN river.display is NOT NULL THEN river.display
-    ELSE station.name
-    END AS group_name,
-    CASE WHEN river.display is NOT NULL THEN river.slug
-    ELSE NULL
-    END AS river_slug
+    CASE WHEN river.display is NOT NULL THEN river.display ELSE station.name END AS group_name,
+    CASE WHEN river.display is NOT NULL THEN river.slug ELSE NULL END AS river_slug,
+    river_station.order AS station_order,
+    0 AS is_downstream
     FROM river
     RIGHT JOIN river_station ON river_station.slug = river.slug
     RIGHT JOIN station ON river_station.station_id = station.id
-    WHERE river.slug LIKE $1 OR river.slug LIKE $2 OR river.slug LIKE $3
-    ORDER BY group_name, river_station.order 
+    WHERE river.slug LIKE $1 OR river.slug LIKE $2 OR river.slug LIKE $3)
+    UNION ALL
+    (SELECT station.name,
+    station.id,
+    station.state,
+    station.value,
+    station.value_downstream,
+    station.value_1hr,
+    station.value_6hr,
+    station.value_24hr,
+    station.value_date,
+    station.type_name AS type,
+    CASE WHEN river.display is NOT NULL THEN river.display ELSE station.name END AS group_name,
+    CASE WHEN river.display is NOT NULL THEN river.slug ELSE NULL END AS river_slug,
+    river_station.order AS station_order,
+    1 AS is_downstream
+    FROM river
+    RIGHT JOIN river_station ON river_station.slug = river.slug
+    RIGHT JOIN station ON river_station.station_id = station.id
+    WHERE (river.slug LIKE $1 OR river.slug LIKE $2 OR river.slug LIKE $3) AND station.type = 'm')
+    ORDER BY group_name, station_order, is_downstream
     `, [`%-${slug}%`, `%${slug}-%`, slug])
     // Address this in SQL/Materialised view
     if (response.rows && [...new Set(response.rows.map(item => item.river_slug))].length > 1) {
