1b71fbb41289ebfb2a7d4198396a8066f40da995
danleech-defra <dan.leech@defra.net>
2021-08-23 09:00:40 +0100

    Stuff

--- app/models/levels.js
+++ app/models/levels.js
@@ -5,19 +5,22 @@ class Levels {
   constructor (query, place, river, levels) {
     this.queryTerm = query.term
     this.queryType = query.type
-    this.filterTypes = (query.filterTypes || 'river,tide,groundwater,rainfall').split(',')
+    this.filterTypes = query.filterTypes ? query.filterTypes.split(',') : []
     this.place = place
     this.river = river
-    // this.types = [...new Set(levels.map(item => item.type))]
     this.types = [...new Set(levels.map(item => item.type))].map(item => (
       { type: item, count: levels.filter(level => level.type === item).length }
     ))
-    this.numLevels = levels.filter(level => this.filterTypes.includes(level.type)).length
+    this.numLevels = this.filterTypes.length ? levels.filter(
+      level => this.filterTypes.includes(level.type)
+    ).length : levels.length
     this.levels = this.createLevels(levels, this.filterTypes)
   }
 
   createLevels (levels, filterTypes) {
-    levels = levels.filter(level => filterTypes.includes(level.type))
+    if (this.filterTypes.length) {
+      levels = levels.filter(level => filterTypes.includes(level.type))
+    }
     const groups = utils.groupBy(levels, 'group_name')
     Object.entries(groups).forEach(([key, value]) => {
       value.forEach((item, index) => {
