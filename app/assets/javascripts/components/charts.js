9ace3bb6fa186ed152f0dbe59cf2d19560f424f9
danleech-defra <dan.leech@defra.net>
2021-11-10 09:57:32 +0000

    Stuff

--- app/assets/javascripts/components/charts.js
+++ app/assets/javascripts/components/charts.js
@@ -458,8 +458,8 @@ function LineChart (containerId, data) {
 }
 
 function BarChart (containerId, data) {
-  // const chart = document.getElementById(containerId)
-  console.log(containerId)
+  const chart = document.getElementById(containerId)
+  console.log(chart)
 }
 
 window.flood.charts = {
