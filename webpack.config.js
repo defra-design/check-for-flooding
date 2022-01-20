dfb482248d8a9ffb6cb3cbbf5a546c3e406ef493
danleech-defra <dan.leech@defra.net>
2022-01-20 09:07:22 +0000

    Stuff

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = (env, argv) => ({
     rules: [
       {
         // Default exclude removes all node_modules but d3 is now distributed es6 so include d3 (& our own src) in transpile
-        include: mPath => mPath.indexOf('app/assets') > -1 || mPath.indexOf('node_modules/d3') > -1 || mPath.indexOf('node_modules/ol') > -1,
+        include: mPath => mPath.indexOf('app/assets') > -1 || mPath.indexOf('node_modules/d3') > -1 || mPath.indexOf('node_modules/internmap') > -1 || mPath.indexOf('node_modules/ol') > -1,
         use: {
           loader: 'babel-loader',
           options: {
