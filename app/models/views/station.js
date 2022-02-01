17d71c6696df407b8b8a539415f1e591674450ad
danleech-defra <dan.leech@defra.net>
2022-02-01 12:35:08 +0000

    Stuff

--- app/models/views/station.js
+++ app/models/views/station.js
@@ -5,13 +5,13 @@ class ViewModel {
   constructor (station, telemetry, place) {
     this.title = (() => {
       if (station.type === 'rainfall') {
-        return `Rainfall at ${station.name}`
+        return `Rainfall at ${station.name} gauge`
       } else if (station.type === 'river') {
-        return `${station.river} level at ${station.name}`
+        return `${station.river} level at ${station.name} gauge`
       } else if (station.type === 'tide') {
-        return station.river ? `${station.river} level at ${station.name}` : `Sea level at ${station.name}`
+        return station.river ? `${station.river} level at ${station.name} gauge` : `Sea level at ${station.name} gauge`
       } else {
-        return `${station.type.charAt(0).toUpperCase() + station.type.slice(1)} level at ${station.name}`
+        return `${station.type.charAt(0).toUpperCase() + station.type.slice(1)} level at ${station.name} borehole`
       }
     })()
     this.station = station
