1174503ba104b63add3cff9d80b64e8254c4580f
danleech-defra <dan.leech@defra.net>
2022-03-30 14:14:31 +0100

    Stuff

--- clock.js
+++ clock.js
@@ -9,7 +9,7 @@ const WORKER_QUEUE = 'worker-queue' // To consume from worker process
 const JOBS = [{ // You could store these jobs in a database
   name: 'Cron process 1',
   message: { taskName: 'updateReadings', queue: 'worker-queue' }, // message in json format
-  cronTime: '0,15,30,45 * * * *', // Every 15 minutes
+  cronTime: '1,16,31,46 * * * *', // Every 15 minutes
   repeat: 1
 }]
 
