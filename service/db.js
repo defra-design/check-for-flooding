9a09aaca41f563a7b365596b4787fab393f3f1f6
danleech-defra <dan.leech@defra.net>
2022-04-13 12:44:03 +0100

    Stuff

--- service/db.js
+++ service/db.js
@@ -1,13 +1,10 @@
 const pgp = require('pg-promise')()
 const connectionString = process.env.DATABASE_URL
-// const connection = {
-//   connectionString: connectionString,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// }
 const connection = {
-  connectionString: connectionString
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
 }
 
 module.exports = pgp(connection)
