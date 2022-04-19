6385f5d96e55bdbc57a4e4770e439ad635f8f396
danleech-defra <dan.leech@defra.net>
2022-04-19 09:48:29 +0100

    Stuff

--- service/bin/update-warnings.js
+++ service/bin/update-warnings.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config({ path: './.env' })
 const db = require('../db')
+const pgp = require('pg-promise')()
 const moment = require('moment-timezone')
 const axios = require('axios')
 const axiosRetry = require('axios-retry')
@@ -26,17 +27,29 @@ module.exports = async (processStartDatetime) => {
     }
   })
   if (response.status === 200 && response.data) {
+    const warnings = []
+    for (const item of response.data.items) {
+      warnings.push({
+        id: item['@id'].substring(item['@id'].lastIndexOf('floods/') + 7),
+        name: item.eaAreaName,
+        message: item.message,
+        severity: item.severityLevel,
+        raised_date: item.timeRaised,
+        message_changed_date: item.timeMessageChanged,
+        severity_changed_date: item.timeSeverityChanged,
+        process_datetime: processStartDatetime.format()
+      })
+    }
     await db.none('TRUNCATE TABLE warning;')
-    await db.none(`
-      INSERT INTO warning (id, name, message, severity, raised_date, message_changed_date, severity_changed_date)
-      values($1, $2)
-    `, [response.data, processStartDatetime])
-    console.log('--> Data update: Updated FGS statement')
+    const cs = new pgp.helpers.ColumnSet(['id', 'name', 'message', 'severity', 'raised_date', 'message_changed_date', 'severity_changed_date', 'process_datetime'], { table: 'warning' })
+    const query = pgp.helpers.insert(warnings, cs)
+    await db.none(query)
+    console.log('--> Data update: Updated flood Alerts and Warnings')
     // Update log
     await db.query('INSERT INTO log (datetime, message) values($1, $2)', [
-      moment().format(), 'Data update: Updated FGS statement'
+      moment().format(), 'Data update: Updated flood Alerts and Warnings'
     ])
   } else {
-    console.log('--> Data update: Error receiving latest FGS')
+    console.log('--> Data update: Error receiving latest flood Alerts and Warnings')
   }
 }
