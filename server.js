96b9938663f03ba91b19725ff4d4f5e48e8f2555
danleech-defra <dan.leech@defra.net>
2022-06-13 10:41:20 +0100

    Stuff

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ if (useCookieSessionStore === 'true') {
   })))
 }
 
-// DBL: Added to enable authenticated calls to backend from app
+// DBL: Added to enable authenticated calls to service from app
 if (isSecure) {
   app.use(cors({
     origin: [process.env.APP_URL],
@@ -134,6 +134,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
+// DBL: Added to enable authenticated calls to service from app
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', process.env.APP_URL)
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 // Prevent search indexing
 app.use(function (req, res, next) {
