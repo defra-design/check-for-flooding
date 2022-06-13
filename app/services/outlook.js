b51ea4fc3f93f615a48c2000a4c1bec0dcb9e706
danleech-defra <dan.leech@defra.net>
2022-06-13 14:06:11 +0100

    Stuff

--- app/services/outlook.js
+++ app/services/outlook.js
@@ -11,12 +11,11 @@ module.exports = {
         //   username: process.env.USERNAME,
         //   password: process.env.PASSWORD
         // },
-        // headers: { Cookie: "cookie1=value; cookie2=value; cookie3=value;" },
+        headers: { Cookie: req.headers.cookie },
         baseURL: serviceUrl
       })
-      console.log(req.cookies)
-      console.log(req.headers.cookie)
-      // console.log(JSON.stringify(req.headers).cookie)
+      // console.log(req.headers.cookie)
+      // console.log(JSON.stringify(response.headers))
       return response
     } catch (error) {
       console.log(error)
