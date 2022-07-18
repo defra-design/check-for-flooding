1a2b1de7ec979766f0c605a3104eee51dcd6c50c
danleech-defra <dan.leech@defra.net>
2022-07-18 17:20:57 +0100

    Stuff

--- service/map.js
+++ service/map.js
@@ -56,7 +56,7 @@ module.exports = {
     response.forEach(item => {
       features.push({
         type: 'Feature',
-        id: item.type === 'R' ? `s${item.station_id}` : `r${item.rloi_id}`,
+        id: item.type === 'R' ? `r${item.station_id}` : `s${item.rloi_id}`,
         geometry: {
           type: 'Point',
           coordinates: [item.lon, item.lat]
