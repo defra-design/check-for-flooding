05770d1c561bdecd4cef0e1e193963a1137ed2f6
danleech-defra <dan.leech@defra.net>
2022-07-20 13:50:20 +0100

    Stuff

--- service/warning.js
+++ service/warning.js
@@ -11,16 +11,16 @@ module.exports = {
   getWarningsWithinBbox: async (bbox = []) => {
     const queryAll = `
       SELECT id, name, severity, message_changed_date AT TIME ZONE '+00' AS updated FROM warning
-      ORDER BY severity ASC
+      WHERE severity > 0 ORDER BY severity ASC
     `
     const queryBbox = `
       (SELECT warning.id, warning.name, warning.severity AS severity, warning.message_changed_date AT TIME ZONE '+00' AS updated FROM warning
       LEFT JOIN flood_alert_areas ON flood_alert_areas.fws_tacode = warning.id
-      WHERE ST_Intersects(ST_MakeEnvelope($1,$2,$3,$4,4326), flood_alert_areas.geom))
+      WHERE ST_Intersects(ST_MakeEnvelope($1,$2,$3,$4,4326), flood_alert_areas.geom) AND warning.severity > 0)
       UNION ALL
       (SELECT warning.id, warning.name, warning.severity AS severity, warning.message_changed_date AT TIME ZONE '+00' AS updated FROM warning
       LEFT JOIN flood_warning_areas ON flood_warning_areas.fws_tacode = warning.id
-      WHERE ST_Intersects(ST_MakeEnvelope($1,$2,$3,$4,4326), flood_warning_areas.geom))
+      WHERE ST_Intersects(ST_MakeEnvelope($1,$2,$3,$4,4326), flood_warning_areas.geom) AND warning.severity > 0)
       ORDER BY severity;  
     `
     const response = bbox.length ? await db.query(queryBbox, bbox) : await db.query(queryAll)
