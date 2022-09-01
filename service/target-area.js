f52ac68907c2bcadbb43ab7d23563fd72bef0060
danleech-defra <dan.leech@defra.net>
2022-09-01 09:22:39 +0100

    Stuff

--- service/target-area.js
+++ service/target-area.js
@@ -25,10 +25,10 @@ module.exports = {
     'river_name', mwl.river_name
     )) filter (WHERE mwl.rloi_id IS NOT NULL), '[]'::json) AS trigger_levels
     FROM flood_warning_areas ta1
-    LEFT JOIN targetarea_trigger tt ON ta1.fws_tacode = tt.targetarea_id
+    LEFT JOIN targetarea_trigger tt ON LOWER(ta1.fws_tacode) = LOWER(tt.targetarea_id)
     LEFT JOIN measure_with_latest mwl ON mwl.rloi_id = tt.trigger_id
-    LEFT JOIN warning w1 ON ta1.fws_tacode = w1.id
-    LEFT JOIN warning w2 ON ta1.parent = w2.id
+    LEFT JOIN warning w1 ON LOWER(ta1.fws_tacode) = LOWER(w1.id)
+    LEFT JOIN warning w2 ON LOWER(ta1.parent) = LOWER(w2.id)
     WHERE LOWER(ta1.fws_tacode) = LOWER($1)
     GROUP BY tt.targetarea_id, ta1.fws_tacode, ta1.ta_name, ta1.descrip, ta1.geom, w1.severity, w1.message, w1.message_changed_date, ta1.parent, w2.severity
     UNION ALL
@@ -54,9 +54,9 @@ module.exports = {
     'river_name', mwl.river_name
     )) filter (WHERE mwl.rloi_id IS NOT NULL), '[]'::json) AS trigger_levels
     FROM flood_alert_areas ta2
-    LEFT JOIN targetarea_trigger tt ON ta2.fws_tacode = tt.targetarea_id
+    LEFT JOIN targetarea_trigger tt ON LOWER(ta2.fws_tacode) = LOWER(tt.targetarea_id)
     LEFT JOIN measure_with_latest mwl ON mwl.rloi_id = tt.trigger_id
-    LEFT JOIN warning w ON ta2.fws_tacode = w.id
+    LEFT JOIN warning w ON LOWER(ta2.fws_tacode) = LOWER(w.id)
     WHERE LOWER(ta2.fws_tacode) = LOWER($1)
     GROUP BY tt.targetarea_id, ta2.fws_tacode, ta2.ta_name, ta2.descrip, ta2.geom, w.severity, w.message, w.message_changed_date);
     `, [id])
