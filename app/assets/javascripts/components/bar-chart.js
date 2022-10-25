c95a3f9d5d8459e680c6859ce891b0bf4d72b2bc
danleech-defra <dan.leech@defra.net>
2022-10-25 15:48:42 +0100

    Stuff

--- app/assets/javascripts/components/bar-chart.js
+++ app/assets/javascripts/components/bar-chart.js
@@ -432,8 +432,8 @@ function BarChart (containerId, stationId, data) {
   const pageBackIcon = document.createElement('span')
   pageBackIcon.setAttribute('class', 'defra-chart-controls-button__icon')
   pageBackIcon.innerHTML = `
-  <svg xmlns="http://www.w3.org/2000/svg" height="13" width="15" aria-hidden="true" focusable="false" viewBox="0 0 15 13">
-    <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path>
+  <svg focusable="false" aria-hidden="true" height="13" width="15" viewBox="0 0 15 13">
+    <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z" fill="currentColor"></path>
   </svg>
   `
   pageBack.appendChild(pageBackIcon)
@@ -459,8 +459,8 @@ function BarChart (containerId, stationId, data) {
   const pageForwardIcon = document.createElement('span')
   pageForwardIcon.setAttribute('class', 'defra-chart-controls-button__icon')
   pageForwardIcon.innerHTML = `
-  <svg xmlns="http://www.w3.org/2000/svg" height="13" width="15" aria-hidden="true" focusable="false" viewBox="0 0 15 13">
-    <path d="m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z"></path>
+  <svg focusable="false" aria-hidden="true" height="13" width="15" viewBox="0 0 15 13">
+    <path d="m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z" fill="currentColor"></path>
   </svg>
   `
   pageForward.appendChild(pageForwardIcon)
@@ -566,6 +566,7 @@ function BarChart (containerId, stationId, data) {
     const keys = ['Tab']
     if (!(e.target.getAttribute('role') === 'cell' && keys.includes(e.key))) return
     e.preventDefault()
+    swapCell(e)
     showTooltip()
   })
 
