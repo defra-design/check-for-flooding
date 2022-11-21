7810e572c80ebbcafdccc1fbde7ed9abf6efd5a1
danleech-defra <dan.leech@defra.net>
2022-11-21 17:53:23 +0000

    Stuff

--- app/models/views/station.js
+++ app/models/views/station.js
@@ -43,11 +43,11 @@ class ViewModel {
       })()
       this.infoState = (() => {
         if (station.latestState === 'high') {
-          return 'The latest level is above the normal range.'
+          return 'The latest level is above the normal range. We calculate the normal range using an average of past measurements and other local factors.'
         } else if (station.latestState === 'low') {
-          return 'The latest level is below the normal range.'
+          return 'The latest level is below the normal range. We calculate the normal range using an average of past measurements and other local factors.'
         } else {
-          return 'The latest level is within the normal range.'
+          return 'The latest level is within the normal range. We calculate the normal range using an average of past measurements and other local factors.'
         }
       })()
     }
