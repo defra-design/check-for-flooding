262669df9346d0a2ab4212da9b5e94571be328f2
Danny Leech <dan.leech@defra.net>
2024-08-21 13:32:37 +0100

    Second threshold order fix

--- service/threshold.js
+++ service/threshold.js
@@ -4,39 +4,41 @@ module.exports = {
   // Used on list page
   getThresholds: async (id) => {
     const response = await db.query(`
-    SELECT * FROM (SELECT DISTINCT ON (targetarea_id) * FROM (
-    SELECT t.id::text, s.name AS name, w.ta_name AS description, z.severity::integer, t.targetarea_id,
-    CASE when t.stage = 'd' THEN concat(t.station_id, '-downstage') ELSE t.station_id END AS station_id,
-    'warning' AS type, t.type AS threshold, t.height::decimal AS value, null::timestamp AS date
-    FROM trigger t
-    LEFT JOIN flood_warning_areas w ON lower(t.targetarea_id) = lower(w.fws_tacode)
-    LEFT JOIN short_ta_name s ON lower(t.targetarea_id) = lower(s.fws_tacode)
-    LEFT JOIN warning z ON lower(t.targetarea_id) = lower(z.id)
-    WHERE t.station_id = $1 AND w.ta_name IS NOT NULL AND t.type SIMILAR TO '(FW RES FW|FW ACT FW|FW ACTCON FW)%'
-    ORDER BY array_position(array['FW RES FW', 'FW ACT FW', 'FW ACTCON FW'], t.type), t.height asc ) w
-    ORDER BY targetarea_id, array_position(array['FW RES FW', 'FW ACT FW', 'FW ACTCON FW'], threshold)) wa
+    SELECT * FROM (
+      SELECT DISTINCT ON (targetarea_id) * FROM (
+      SELECT t.id::text, s.name AS name, w.ta_name AS description, z.severity::integer, t.targetarea_id,
+      CASE when t.stage = 'd' THEN concat(t.station_id, '-downstage') ELSE t.station_id END AS station_id,
+      'warning' AS type, t.type AS threshold, t.height::decimal AS value, null::timestamp AS date
+      FROM trigger t
+      LEFT JOIN flood_warning_areas w ON lower(t.targetarea_id) = lower(w.fws_tacode)
+      LEFT JOIN short_ta_name s ON lower(t.targetarea_id) = lower(s.fws_tacode)
+      LEFT JOIN warning z ON lower(t.targetarea_id) = lower(z.id)
+      WHERE t.station_id = $1 AND w.ta_name IS NOT NULL AND t.type SIMILAR TO '(FW RES FW|FW ACT FW|FW ACTCON FW)%'
+      ) w
+      ORDER BY targetarea_id, array_position(array['FW RES FW', 'FW ACT FW', 'FW ACTCON FW'], threshold), value asc
+    ) wa
+    UNION (SELECT * FROM (
+      SELECT DISTINCT ON (targetarea_id) * FROM (
+      SELECT t.id::text, s.name AS name, a.ta_name AS description, z.severity::integer, t.targetarea_id,
+      CASE when t.stage = 'd' THEN concat(t.station_id, '-downstage') ELSE t.station_id END AS station_id,
+      'alert' AS type, t.type AS threshold, t.height::decimal AS value, null::timestamp AS date
+      FROM trigger t
+      LEFT JOIN flood_alert_areas a ON lower(t.targetarea_id) = lower(a.fws_tacode)
+      LEFT JOIN short_ta_name s ON lower(t.targetarea_id) = lower(s.fws_tacode)
+      LEFT JOIN warning z ON lower(t.targetarea_id) = lower(z.id)
+      WHERE t.station_id = $1 AND a.ta_name IS NOT NULL AND t.type SIMILAR TO '(FW RES FAL|FW ACT FAL|FW ACTCON FAL)%'
+      ) a
+      ORDER BY targetarea_id, array_position(array['FW RES FW', 'FW ACT FW', 'FW ACTCON FW'], threshold), value asc
+    ) aa )
     UNION (
-    SELECT * FROM (SELECT DISTINCT ON (targetarea_id) * FROM (
-    SELECT t.id::text, s.name AS name, a.ta_name AS description, z.severity::integer, t.targetarea_id,
-    CASE when t.stage = 'd' THEN concat(t.station_id, '-downstage') ELSE t.station_id END AS station_id,
-    'alert' AS type, t.type AS threshold, t.height::decimal AS value, null::timestamp AS date
-    FROM trigger t
-    LEFT JOIN flood_alert_areas a ON lower(t.targetarea_id) = lower(a.fws_tacode)
-    LEFT JOIN short_ta_name s ON lower(t.targetarea_id) = lower(s.fws_tacode)
-    LEFT JOIN warning z ON lower(t.targetarea_id) = lower(z.id)
-    WHERE t.station_id = $1 AND a.ta_name IS NOT NULL AND t.type SIMILAR TO '(FW RES FAL|FW ACT FAL|FW ACTCON FAL)%'
-    ORDER BY array_position(array['FW RES FAL', 'FW ACT FAL', 'FW ACTCON FAL'], t.type), t.height asc ) a
-    ORDER BY targetarea_id, array_position(array['FW RES FW', 'FW ACT FW', 'FW ACTCON FW'], threshold)) aa
+      SELECT concat(rloi_id, '-max') AS id, null AS name, null AS description, null AS severity, null AS targetarea_id, rloi_id AS station_id, 'max' AS type, null AS threshold, level_max::decimal AS value, level_max_datetime AS date
+      FROM measure_with_latest
+      WHERE rloi_id = $1
     )
     UNION (
-    SELECT concat(rloi_id, '-max') AS id, null AS name, null AS description, null AS severity, null AS targetarea_id, rloi_id AS station_id, 'max' AS type, null AS threshold, level_max::decimal AS value, level_max_datetime AS date
-    FROM measure_with_latest
-    WHERE rloi_id = $1
-    )
-    UNION (
-    SELECT concat(rloi_id, '-high') AS id, null AS name, null AS description, null AS severity, null AS targetarea_id, rloi_id AS station_id, 'high' AS type, null AS threshold, level_high::decimal AS value, null AS date
-    FROM measure_with_latest
-    WHERE rloi_id = $1
+      SELECT concat(rloi_id, '-high') AS id, null AS name, null AS description, null AS severity, null AS targetarea_id, rloi_id AS station_id, 'high' AS type, null AS threshold, level_high::decimal AS value, null AS date
+      FROM measure_with_latest
+      WHERE rloi_id = $1
     )
     ORDER BY value desc;   
     `, id)
