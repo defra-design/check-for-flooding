93d1c4d62bb23bdf1a8c665a819b2ffd1cba17ab
keyurx11 <keyurx11@gmail.com>
2025-06-09 18:27:26 +0100

    Replace database query in getOutlook with mocked data from JSON file

--- service/outlook.js
+++ service/outlook.js
@@ -3,9 +3,13 @@ const db = require('./db')
 module.exports = {
   // Used on natioanl and location page
   getOutlook: async () => {
-    const response = await db.query(`
-    SELECT * FROM outlook
-    `)
-    return response.length ? response[0].data.statement : {}
-  }
+    // const response = await db.query(`
+    // SELECT * FROM outlook
+    // `)
+    
+  // Mocking the file content instead of pulling from the service
+    const outlook = require('../models/5df.json')
+    // console.log('Mocked Outlook object:', outlook);
+    return Promise.resolve(outlook)
+  },
 }
